819304dc3be6cae19e2f42c47554ead2c5c6af54
hhhgtr00055 <hhhgtr00055@gmail.com>
2026-03-02 00:34:59 +0800

    fix: dynamically set base path for Vercel vs GH Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Ethereum-roadmap/',
+  base: process.env.VERCEL ? '/' : '/Ethereum-roadmap/',
   plugins: [react()],
 })
